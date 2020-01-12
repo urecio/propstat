64d01ce323b1dac3ffdcf3b922c666623b7921e9
Vu Nam Nguyen <vu@strangelab.co.uk>
2020-01-12 12:39:46 +0000

    Add docker-compose and edit data

--- data.sh
+++ data.sh
@@ -1,4 +1,11 @@
-curl https://propstat.s3.eu-west-2.amazonaws.com/ea_floodmapforplanningriversandseafloodzone2_shp_full.zip --output data/ea_floodmapforplanningriversandseafloodzone2_shp_full.zip
-curl https://propstat.s3.eu-west-2.amazonaws.com/ea_floodmapforplanningriversandseafloodzone3_shp_full.zip --output data/ea_floodmapforplanningriversandseafloodzone3_shp_full.zip
-unzip ea_floodmapforplanningriversandseafloodzone2_shp_full.zip -d data/flood2
-unzip ea_floodmapforplanningriversandseafloodzone3_shp_full.zip -d data/flood3
+mkdir -p data
+
+if [ ! -f data/ea_floodmapforplanningriversandseafloodzone2_shp_full.zip ]; then
+  curl https://propstat.s3.eu-west-2.amazonaws.com/ea_floodmapforplanningriversandseafloodzone2_shp_full.zip --output data/ea_floodmapforplanningriversandseafloodzone2_shp_full.zip
+  unzip data/ea_floodmapforplanningriversandseafloodzone2_shp_full.zip -d data/flood2
+fi
+
+if [ ! -f data/ea_floodmapforplanningriversandseafloodzone3_shp_full.zip ]; then
+  curl https://propstat.s3.eu-west-2.amazonaws.com/ea_floodmapforplanningriversandseafloodzone3_shp_full.zip --output data/ea_floodmapforplanningriversandseafloodzone3_shp_full.zip
+  unzip data/ea_floodmapforplanningriversandseafloodzone3_shp_full.zip -d data/flood3
+fi
