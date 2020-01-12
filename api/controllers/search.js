b3b03ed3541069880e62f7654f0e5bc0c51bd055
urecio <crustaceo1@gmail.com>
2020-01-12 18:49:33 +0100

    Uses nominatim on search endpoint

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -1,7 +1,25 @@
+const fetch = require('node-fetch');
 
-function search (ctx) {
-  // let searchQuery = ctx.request.query;
-  ctx.ok({ addresses: [{ address: '', geojson: {}, id: 1 }] });
+const NOMINATIM_PORT = process.env.NOMINATIM_PORT;
+const NOMINATIM_URL = `${process.env.NOMINATIM_HOST}${NOMINATIM_PORT ? ':' + NOMINATIM_PORT : ''}`;
+
+const search  = async (ctx) => {
+  const searchQuery = ctx.query.q;
+
+  if (!searchQuery) {
+    ctx.throw(400, 'Please, provide a search query');
+  }
+
+  try {
+
+    const res = await fetch(`http://${NOMINATIM_URL}/search?q="${searchQuery}"&format=json`);
+    const body = await res.json();
+    ctx.ok(body);
+
+  } catch (e) {
+    console.error('Error fetching from Nominatim');
+    ctx.throw(500, 'Sorry, the service is not working at the moment, try again later');
+  }
 }
 
 module.exports = {
