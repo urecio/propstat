c004265a952d14bbdc7006a234e0b4a78c2dd428
Vu Nam Nguyen <vu@strangelab.co.uk>
2020-01-12 12:23:56 +0000

    Add floodzone data

---
+++ data.sh
@@ -0,0 +1,4 @@
+curl https://propstat.s3.eu-west-2.amazonaws.com/ea_floodmapforplanningriversandseafloodzone2_shp_full.zip --output data/ea_floodmapforplanningriversandseafloodzone2_shp_full.zip
+curl https://propstat.s3.eu-west-2.amazonaws.com/ea_floodmapforplanningriversandseafloodzone3_shp_full.zip --output data/ea_floodmapforplanningriversandseafloodzone3_shp_full.zip
+unzip ea_floodmapforplanningriversandseafloodzone2_shp_full.zip -d data/flood2
+unzip ea_floodmapforplanningriversandseafloodzone3_shp_full.zip -d data/flood3
